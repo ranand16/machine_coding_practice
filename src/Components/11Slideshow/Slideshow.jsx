d5d58903e03a1279f2f6825a488f52dab8b9c78d
Rishabh Anand <ranand16@gmail.com>
2024-03-02 09:54:05 +0530

    slideshow

--- src/Components/11Slideshow/Slideshow.jsx
+++ src/Components/11Slideshow/Slideshow.jsx
@@ -5,17 +5,16 @@ import styles from './styles.css'
 const Slideshow = ({ images }) => {
     const [active, setActive] = useState(0);
     return <div>
-        <div className="imagecontainer">{
-            images.map((imgobj, i)=>{
-                console.log(imgobj);
-                return <div onClick={()=>setActive(i)} key={imgobj["title"]} style={{ width: "150px", background: `url(${imgobj["image_url"]}) center center / contain no-repeat #aaa` }}></div>
-            })
-        }</div>
-        <div className="slidercontainer">
-            <div style={{  }} onClick={()=> active < 1 ? ()=>{} : setActive(i-1)} >Previous</div>
-            <div style={{  }}>Next</div>
+        <div className="imagecontainer">
+            <div className="nav-btns nav-btns-prev" onClick={()=> active < 1 ? ()=>{} : setActive((i) => i-1)}>Previous</div>
+            <div className="nav-btns nav-btns-next" onClick={()=> active >= images.length-1 ? ()=>{} : setActive((i) => i+1)}>Next</div>
             {
-                images.map((imgobj)=><Slide key={imgobj["title"]} title={imgobj["title"]} image={imgobj["image_url"]} active={imgobj["title"] === active} />)
+                images.map((imgobj, i)=><div onClick={()=>setActive(i)} key={imgobj["title"]} style={{ width: '100px', background: `url(${imgobj["image_url"]}) center center / contain no-repeat #aaa` }}></div>)
+            }
+        </div>
+        <div className="slidercontainer">
+            {
+                images.map((imgobj, i)=><Slide key={imgobj["title"]} title={imgobj["title"]} image={imgobj["image_url"]} active={i === active} />)
             }
         </div>
     </div>;
